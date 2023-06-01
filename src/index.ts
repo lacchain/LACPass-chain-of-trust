c4a0c17ba8a0abc5f901d27a07757217c5b3cdf0
eum602 <eum602@gmail.com>
2023-05-31 19:10:08 -0500

    chore: expose interface

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './interfaces/manager/manager';
 export { INewAccountIdAttribute } from 'lacpass-identity';
 export { ManagerService } from './services/manager';
 export { ErrorsMessages } from './constants/errorMessages';
+export * from './dto/lac1/managerDTO';
