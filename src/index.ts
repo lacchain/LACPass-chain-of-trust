e61a61704648471723c142a53c596e4ab884e5ac
eum602 <eum602@gmail.com>
2023-05-31 22:48:25 -0500

    chore: update Logger identifier, update identity package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from './interfaces/manager/manager';
-export { INewAccountIdAttribute } from 'lacpass-identity';
+export { INewAccountIdAttribute, DidEntity } from 'lacpass-identity';
 export { ManagerService } from './services/manager';
 export { ErrorsMessages } from './constants/errorMessages';
 export * from './dto/lac1/managerDTO';
