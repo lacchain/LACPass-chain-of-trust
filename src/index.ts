621479f4fd8740f4294bb46b9721e0964cc13770
eum602 <eum602@gmail.com>
2023-05-31 19:38:20 -0500

    chore: export entity

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { INewAccountIdAttribute } from 'lacpass-identity';
 export { ManagerService } from './services/manager';
 export { ErrorsMessages } from './constants/errorMessages';
 export * from './dto/lac1/managerDTO';
+export { Manager as ManagerEntity } from './entities/manager.entity';
