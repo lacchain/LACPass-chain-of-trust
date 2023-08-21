7b4a308da31235a52be1265cd6d5a95d5d166fbc
eum602 <eum602@gmail.com>
2023-08-21 00:13:13 -0500

    expose methods to resolve public directory and chain of trust contract addresses

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,7 @@ export { ManagerService } from './services/manager';
 export { ErrorsMessages } from './constants/errorMessages';
 export * from './dto/lac1/managerDTO';
 export { Manager as ManagerEntity } from './entities/manager.entity';
+export {
+  resolvePublicDirectoryAddress,
+  resolveChainOfTrustAddress
+} from './config/index';
